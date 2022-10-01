c03bbef869ab80d348dd3a986da4bfb7b68097f5
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-10-01 13:32:00 +0530

    Initial commit with firebase initialized with auth and data storage. few hooks made in earlier projects like useAuth etc is also copied in this project to use.

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth/authmanager'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeZu7wKyvMv15bo_MR7XSeEPGxdVs3FCs",
+    authDomain: "thecollabsite.firebaseapp.com",
+    projectId: "thecollabsite",
+    storageBucket: "thecollabsite.appspot.com",
+    messagingSenderId: "1079156643343",
+    appId: "1:1079156643343:web:165e426947d06b439ff57a"
+}
+
+//init firebase
+firebase.initialize(firebaseConfig)
+
+
+//init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
