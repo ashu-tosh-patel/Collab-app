0291112c62259d4efebc7316f21b3b2722e2640d
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-10-08 01:06:08 +0530

    implemented uselogin hook to login a user and set the online status to be true and also modified the router components to show only required options in the navbar and redirect user to dashboard after login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 //styles & images
 import './Navbar.css'
@@ -6,6 +8,10 @@ import Temple from '../assets/temple.svg'
 
 
 export default function Navbar() {
+
+    const { logout, isPending } = useLogout()
+    const { user } = useAuthContext()
+
     return (
         <div className="navbar">
             <ul>
@@ -13,11 +19,13 @@ export default function Navbar() {
                     <img src={Temple} alt="dojo logo" />
                     <span>The Collab</span>
                 </li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">SignUp</Link></li>
-                <li>
-                    <button className="btn">Logout</button>
-                </li>
+                {!user && <li><Link to="/login">Login</Link></li>}
+                {!user && <li><Link to="/signup">SignUp</Link></li>}
+
+                {user && <li>
+                    {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+                    {isPending && <button className="btn" disabled>Logging out...</button>}
+                </li>}
             </ul>
         </div>
     )
