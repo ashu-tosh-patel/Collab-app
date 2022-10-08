96d46bac726bf0e2e14286f65389e555891e4e45
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-10-08 16:42:47 +0530

    now the app uploads new project object on firestore in the documents folder

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,7 +1,12 @@
+import { useCollection } from '../../hooks/useCollection'
+
 //styles
 import './Dashboard.css'
 
 export default function dashboard() {
+
+    const 
+
     return (
         <div>
             Dashboard
